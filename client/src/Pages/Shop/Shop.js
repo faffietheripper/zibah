05fd582893e9fe35b56232330ff92819eea3517f
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-07 01:36:05 +0100

    cart edits

--- client/src/Pages/Shop/Shop.js
+++ client/src/Pages/Shop/Shop.js
@@ -4,12 +4,18 @@ import { useCart } from "react-use-cart";
 
 export default function Shop({ products }) {
   const { addItem } = useCart();
+
   return (
     <div>
       <ShopHero />
       <section className="flex place-content-around m-6 flex-wrap my-8">
         {products.map((product) => {
           console.log(product);
+          function handleClick() {
+            addItem({ ...product, id: product._id });
+            alert("The selected product has been added to cart");
+          }
+
           return (
             <div className="product m-1  border-solid border-black rounded">
               <section className="block group m-1">
@@ -18,10 +24,7 @@ export default function Shop({ products }) {
                 <p>{product.description}</p>
                 <p className="mt-1 text-sm text-red-700">{product.price}</p>
 
-                <button
-                  className="block w-full rounded bg-teal-600 p-4 text-sm font-medium transition hover:scale-105"
-                  onClick={() => addItem({ ...product, id: product._id })}
-                >
+                <button className="block w-full rounded bg-teal-600 p-4 text-sm font-medium transition hover:scale-105" onClick={handleClick}>
                   Add to cart
                 </button>
               </section>
