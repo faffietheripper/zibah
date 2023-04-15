ee29a635a18983b4f44c269f38caed07a1d48fea
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-15 08:08:20 +0100

    Mobile responsiveness

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -6,11 +6,11 @@ export default function Footer() {
     <div class="pt-20">
       <p>
         <footer aria-label="Site Footer" className="bg-white lg:grid lg:grid-cols-5">
-          <div className="relative block h-32 lg:col-span-2 lg:h-full">
+          <div className=" block h-32 lg:col-span-2 lg:h-full">
             <img
               src="https://lh3.googleusercontent.com/pw/AMWts8DUFJLFRAok6ih_vfBFzMx9CW6G2ewR9cFf6NFrG-uO32SYXGcjrvZ-NZND5jJbUV7v_d3P9q6IECnBJ9RZOM-1ysYDCTLtEqB8B7z9f3zYtTf9gq-J_s9wMzwOt87yyDd-B2s5wI-5c9QEu3vftjZC0SEUTs-VJf5lY8FBvXnXipedUCktw8ktQB6Q70EbJ_LrIsJD-VCSSQsVf7n6-bnE4LJelr5UNIfRQrIIabai9HEeYllzUrO1Rx2q4ES1AbgnMST3BmmrVFnD90rPbVg3G9mmpx6I4K64Sj_93aXHlyCm6sJfaXZq9i1yZVYQj6wN8SGkGAcF88V3F9jEy8jTQBzjsnep95ReIMZLiJRwy8fmT554NviyW08AOv_BnBSOgtMgESJuBSta9mis-047ZpzUz-XviSpKgF-9Oa6cbXocyocIh_SVHeCNNmtapB-J1TdZWoXQh4jdOTpA98s5xmH0q9B680LoTP8HQQxx6OH54kxUmGYL2te_tDjoUVqsPQxhZXhx5AZe4IsIiyphchTDCc2HcCpkSQJcOAh2-dDIx9qPT3fwoaD8HoQ7cLOh6AtKcvYl43imXDYwrOfUd0ELtYYHG3LWiz7J_Ljd2VVJBNZxBjbBH-G5FXAPv4isfUNRV82kubfypXGGHu8DyQoy-0apuy-g7t9yJ7FOBUNvw1sVnGWwq9WPvpv9r48TGpC8VXpn0LJTE2F6PAVyXCUmjWsMHhNf2cEBMNJGPdTMiuaqrEkvknrI1NO2dXnLNEvMmhwh3BZnCPS_4_K_YQM2OMwWLDPvSYjHPYCRtAhAhimWiIDau0TfgOWMXN5HAxH-LrdwLzj-7GjML8cFeY0WABimka8aDkQDkag_WqCDhR3N_K-0ZLukBJOV54wUddnrdL9xJ1zFd7c73drUAhR_BBlbSEzOeqIFze3utkA5GuYztL3NInuHC_3IRsMXqs8=w232-h233-no?authuser=0"
               alt=""
-              className="absolute inset-0 object-cover w-full h-full"
+              className="inset-0 object-cover w-full h-full"
             />
           </div>
 
