513695d9ce560f49eb9e6995c18fd7a299e50139
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-18 02:25:57 +0100

    Add shop hero

--- client/src/Components/ShopHero/ShopHero.js
+++ client/src/Components/ShopHero/ShopHero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ShopHero() {
   return (
     <div>
-      <section class="overflow-hidden bg-[url(https://cdn.pixabay.com/photo/2019/05/28/16/48/business-4235556__480.jpg)] bg-cover bg-top bg-no-repeat h-56 md:h-[40vh] lg:h-[40vh]"></section>
+      <section class="overflow-hidden bg-[url(https://cdn.pixabay.com/photo/2016/11/19/15/40/clothes-1839935_1280.jpg)] bg-cover bg-center bg-no-repeat h-56 md:h-[40vh] lg:h-[40vh]"></section>
     </div>
   );
 }
