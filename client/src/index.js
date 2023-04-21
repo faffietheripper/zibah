b5ab39d0871355f1b0ea28303e0f2abe30a2aec1
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-21 16:17:16 +0100

    change autho id

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,17 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider domain={process.env.REACT_APP_AUTH_DOMAIN} clientId={process.env.REACT_APP_AUTH_CLIENT_ID} redirectUri={window.location.origin}>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: process.env.REACT_APP_AUTH_REDIRECT_URI,
+      }}
+    >
       <App />,
     </Auth0Provider>
   </React.StrictMode>
