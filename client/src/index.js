ef7a1eb31935e97e4d41897152b6f874f21639d2
faffietheripper <121829394+faffietheripper@users.noreply.github.com>
2023-04-21 15:51:48 +0100

    Set app security

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: process.env.REACT_APP_AUTH_REDIRECT_URI,
-      }}
-    >
+    <Auth0Provider domain={process.env.REACT_APP_AUTH_DOMAIN} clientId={process.env.REACT_APP_AUTH_CLIENT_ID} redirectUri={window.location.origin}>
       <App />,
     </Auth0Provider>
   </React.StrictMode>
